46b2025b1df20e6ae1fda596d11cb7543167f228
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-17 15:47:45 +0100

    FEAT - Header is nice

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,47 @@
 <header id="site_header">
-    Site Header
+
+    <div class="topBar bg-primary">
+        <div class="container d-flex justify-content-end ">
+
+            <div class="visaPow me-5">
+                <span>
+                    DC POWER VISA
+                </span>
+            </div>
+
+            <div class="additional me-5">
+                <span>
+                    ADDITIONAL DC SITES
+                </span>
+            </div>
+
+        </div>
+    </div>
+
+    <nav class="nav justify-content-center align-items-center">
+
+        <a class="navbar-brand" href="#">
+            <img src="{{asset ('img/dc-logo.png')}}" alt="" width="80">
+        </a>
+
+        <a class="nav-link" href="#">CHARACTERS</a>
+        <a class="nav-link" href="#">COMICS</a>
+        <a class="nav-link" href="#">MOVIES</a>
+        <a class="nav-link" href="#">TV</a>
+        <a class="nav-link" href="#">GAMES</a>
+        <a class="nav-link" href="#">COLLECTIBLES</a>
+        <a class="nav-link" href="#">VIDEOS</a>
+        <a class="nav-link" href="#">FANS</a>
+        <a class="nav-link" href="#">NEWS</a>
+        <a class="nav-link" href="#">SHOP</a>
+
+
+        <span class="f_group">
+            <input type="text" class="">
+            <!-- magnifier here -->
+        </span>
+
+
+    </nav>
+
 </header>
