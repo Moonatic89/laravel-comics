e3a18419970639ccad65044e2a58429f94e21149
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-17 17:15:52 +0100

    FEAT - Grid Made

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,36 @@
 </div>
 
 
-<div class="issues">
+<div class="issues bg-dark">
 
-    <div class="container">
+    <div class="container pt-5">
+        <div class="row">
 
-        @foreach ($issues as $issue)
-        <h2>{{$issue['title']}}</h2>
-        @endforeach
+            @foreach ($issues as $issue)
+
+            <div class="col-2">
+
+                <div class="myCard">
+
+                    <div class="myCardImg">
+                        <img src="{{$issue['thumb']}}" class="card-img-top" alt="...">
+                    </div>
+
+                    <div class="myCardText text-light">
+                        <h5>{{strtoupper($issue['series'])}}</h5>
+                    </div>
+
+                </div>
+
+            </div>
+            @endforeach
+
+        </div>
+
+
+
+
+
 
     </div>
 
