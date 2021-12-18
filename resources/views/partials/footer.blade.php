f8e963b781392cf6201c5f0e3703524134d72302
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-18 11:30:21 +0100

    FEAT - Footer lower bar made

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,6 +7,23 @@
 
     <div class="contactBar">
 
+        <div class="container w-100 contacts d-flex justify-content-between align-items-center">
+            <button type="button" class="btn btn-outline-primary text-light">SIGN-UP NOW!</button>
+            <div class="contactButtons d-flex align-items-center">
+                <div class="text text-primary pe-5 fw-bold">
+                    <h4>FOLLOW US</h4>
+                </div>
+                <div class="icons">
+                    <a href="" class="pe-3"><img src="{{asset ('img/footer-facebook.png')}}" alt=""></a>
+                    <a href="" class="pe-3"><img src="{{asset ('img/footer-twitter.png')}}" alt=""></a>
+                    <a href="" class="pe-3"><img src="{{asset ('img/footer-youtube.png')}}" alt=""></a>
+                    <a href="" class="pe-3"><img src="{{asset ('img/footer-pinterest.png')}}" alt=""></a>
+                    <a href="" class="pe-3"><img src="{{asset ('img/footer-periscope.png')}}" alt=""></a>
+                </div>
+            </div>
+
+        </div>
+
     </div>
 
 </footer>
