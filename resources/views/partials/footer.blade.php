43fa1d436870fb7430660b19182643b13c3e9828
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-18 12:43:05 +0100

    Footer structure finished

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,6 +2,103 @@
 
     <div class="footerImage">
 
+        <div class="container text-secondary ">
+
+
+            <div class="myRow d-flex justify-content-between">
+
+                <div class="textContent">
+
+
+                    <div class="links d-flex pt-5">
+
+                        <div class="leftList">
+
+                            <span class="text-white fw-bold ">
+                                DC COMICS
+                            </span>
+
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+
+                            <span class="text-white fw-bold ">
+                                SHOP
+                            </span>
+
+                            <ul>
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+
+                        </div>
+
+                        <div class="midList">
+
+                            <span class="text-white fw-bold ">
+                                DC
+                            </span>
+
+                            <ul>
+                                <li>Terms of Use</li>
+                                <li>Privacy Policy (New)</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshop</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+
+
+
+                        </div>
+
+                        <div class="rightList">
+
+                            <span class="text-white fw-bold ">
+                                SITES
+                            </span>
+
+                            <ul>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+
+
+                        </div>
+                    </div>
+
+                    <div class="copyright">
+                        All Site Content TM and COPYRIGHT 2020 DC Entertainment, unless otherwise noted here. All rights
+                        reserved. <br>
+                        Cookies Settings
+                    </div>
+
+                </div>
+
+                <div class="logoBig">
+                    <img src="{{asset ('img/dc-logo-bg.png')}}" alt="" />
+                </div>
+
+            </div>
+
+
+
+
+        </div>
 
     </div>
 
