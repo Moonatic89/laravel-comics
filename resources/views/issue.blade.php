437563f8977c53692a2236a76d2e6054a6c5c6d0
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-20 16:44:21 +0100

    TEST - Styling issue page

--- resources/views/issue.blade.php
+++ resources/views/issue.blade.php
@@ -3,35 +3,38 @@
 @section ('content')
 
 
+<div class="container">
 
-<div class="issueBlock">
+    <div class="issueBlock">
 
-    <div class="issueTitle">
-        <span>
-            {{$comic['title']}}
-        </span>
-
-    </div>
-
-    <div class="pricing">
-        <div class="leftCol">
+        <div class="issueTitle">
             <span>
-                U.S. Price: {{$comic['price']}}
+                {{$comic['title']}}
             </span>
-            <span>
-                AVAILABLE
-            </span>
+
         </div>
 
-        <div class="rightCol">
-            <span>
-                Check Availability
-            </span>
-        </div>
-    </div>
+        <div class="pricing">
+            <div class="leftCol">
+                <span>
+                    U.S. Price: {{$comic['price']}}
+                </span>
+                <span>
+                    AVAILABLE
+                </span>
+            </div>
 
-    <div class="issueDesc">
-        {{$comic['description']}}
+            <div class="rightCol">
+                <span>
+                    Check Availability
+                </span>
+            </div>
+        </div>
+
+        <div class="issueDesc">
+            {{$comic['description']}}
+
+        </div>
 
     </div>
 
@@ -39,73 +42,124 @@
 
 
 <div class="issueArtists">
-    <div class="talents">
+    <div class="container">
 
-        <div class="graphic">
+        <div class="talents">
 
-            <span>
-                Art by:
-            </span>
+            <div class="graphic">
 
-            <span>
-                @foreach ($comic['artists'] as $artist)
-                {{$artist}}
-                @endforeach
-            </span>
-
-        </div>
-
-        <div class="textual">
-
-            <span>
-                Written by:
-            </span>
-
-            <span>
-                @foreach ($comic['artists'] as $artist)
-                {{$artist}}
-                @endforeach
-            </span>
-
-
-        </div>
-
-        <div class="specs">
-
-            <div class="specSeries">
                 <span>
-                    Series:
+                    Art by:
                 </span>
 
                 <span>
-                    {{$comic['series']}}
+                    @foreach ($comic['artists'] as $artist)
+                    {{$artist}}
+                    @endforeach
                 </span>
+
             </div>
 
-            <div class="specPricing">
+            <div class="textual">
+
                 <span>
-                    U.S. Price:
+                    Written by:
                 </span>
 
                 <span>
-                    {{$comic['price']}}
+                    @foreach ($comic['artists'] as $artist)
+                    {{$artist}}
+                    @endforeach
                 </span>
+
+
             </div>
 
-            <div class="specOnSale">
-                <span>
-                    On Sale Date:
-                </span>
+            <div class="specs">
 
-                <!-- WARNING! This should be on date style, not just a number -->
+                <div class="specSeries">
+                    <span>
+                        Series:
+                    </span>
 
-                <span>
-                    {{$comic['sale_date']}}
-                </span>
+                    <span>
+                        {{$comic['series']}}
+                    </span>
+                </div>
+
+                <div class="specPricing">
+                    <span>
+                        U.S. Price:
+                    </span>
+
+                    <span>
+                        {{$comic['price']}}
+                    </span>
+                </div>
+
+                <div class="specOnSale">
+                    <span>
+                        On Sale Date:
+                    </span>
+
+                    <!-- WARNING! This should be on date style, not just a number -->
+
+                    <span>
+                        {{$comic['sale_date']}}
+                    </span>
+                </div>
             </div>
         </div>
 
     </div>
+
+
+    <div class="issueShop">
+        <div class="container">
+            <div class="row">
+                <div class="col d-flex align-items-center border border-light">
+                    <div class="shopImg me-3">
+                        <img src="{{asset ('img/buy-comics-digital-comics.png')}}" class="" alt="">
+                    </div>
+                    <span>
+                        DIGITAL COMICS
+                    </span>
+                </div>
+
+
+                <div class="col d-flex align-items-center border border-light">
+                    <div class="shopImg me-3">
+                        <img src="{{asset ('img/buy-comics-digital-comics.png')}}" class="" alt="">
+                    </div>
+                    <span>
+                        SHOP DC
+                    </span>
+                </div>
+
+
+                <div class="col d-flex align-items-center border border-light">
+                    <div class="shopImg me-3">
+                        <img src="{{asset ('img/buy-comics-digital-comics.png')}}" class="" alt="">
+                    </div>
+                    <span>
+                        COMIC SHOP LOCATOR
+                    </span>
+                </div>
+
+
+                <div class="col d-flex align-items-center border border-light">
+                    <div class="shopImg me-3">
+                        <img src="{{asset ('img/buy-comics-digital-comics.png')}}" class="" alt="">
+                    </div>
+                    <span>
+                        SUBSCRIPTIONS
+                    </span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 
 
     @endsection
