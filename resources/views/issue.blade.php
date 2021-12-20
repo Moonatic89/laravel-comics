420f045f3fad923d8a7721d0029e5e9665ae35a9
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-20 16:03:25 +0100

    TEST - Compiling Issue Page

--- resources/views/issue.blade.php
+++ resources/views/issue.blade.php
@@ -2,8 +2,77 @@
 
 @section ('content')
 
+<div class="issueBlock">
+
+    <div class="issueTitle">
+        <span>
+            {{$comic['title']}}
+        </span>
+
+    </div>
+
+    <div class="pricing">
+        <div class="leftCol">
+            <span>
+                U.S. Price: {{$comic['price']}}
+            </span>
+            <span>
+                AVAILABLE
+            </span>
+        </div>
+
+        <div class="rightCol">
+            <span>
+                Check Availability
+            </span>
+        </div>
+    </div>
+
+    <div class="issueDesc">
+        {{$comic['description']}}
+
+    </div>
+
+</div>
+
+<div class="issueArtists">
+
+    <div class="talents">
+
+        <div class="graphic">
+
+            <span>
+                Art by:
+            </span>
+
+            <span>
+                {{$comic['artists']}}
+            </span>
+
+        </div>
+
+        <div class="textual">
+
+            <span>
+                Written by:
+            </span>
+
+            <span>
+                {{$comic['writers']}}
+            </span>
+
+
+        </div>
+
+    </div>
+
+    <div class="specs">
+
+    </div>
+
+</div>
+
 <p>
-    {{$comic['title']}}
 </p>
 
 
